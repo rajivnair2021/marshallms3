e371ecca2cf9a656cb5fd6778a46b443f5e312e8
rajivnair2021 <rajivnair2021@gmail.com>
2026-05-18 07:38:12 +0530

    first commit

---
+++ lib/auth.ts
@@ -0,0 +1,38 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
+import { env } from "./env";
+import { emailOTP } from "better-auth/plugins";
+import { resend } from "./resend";
+import { admin } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  trustedOrigins: [
+    "http://localhost:3000",                // Local network or web client
+    "http://192.168.1.4:3000",     // Production domain
+               // Example for mobile (e.g., Capacitor/Ionic)
+  ],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  socialProviders: {
+    github: {
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_SECRET,
+    },
+  },
+
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        await resend.emails.send({
+          from: "RajivLMS <onboarding@resend.dev>",
+          to: [email],
+          subject: "RajivLMS - Verify your email",
+          html: `<p>Your OTP is <strong>${otp}</strong></p>`,
+        });
+      },
+    }),
+    admin(),
+  ],
+});
---
+++ next.config.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type { NextConfig } from "next";
+//@ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
+const nextConfig: NextConfig = {
+    crossOrigin: 'anonymous',
+    allowedDevOrigins: ['192.168.1.4:3000','192.168.1.4'],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "phoenix-lms.fly.storage.tigris.dev",
+        // //marshallms
+        port: "",
+        protocol: "https",
+      },
+    ],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
